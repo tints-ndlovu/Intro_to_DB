cc4ece5db1f6a10fe4fe77ad10cafc4b7de7b836
tints-ndlovu <tintswalotndlovu@gmail.com>
2025-08-17 20:09:17 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Switch to the database
+USE alx_book_store;
+
+-- Print full description of the table 'Books'
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
