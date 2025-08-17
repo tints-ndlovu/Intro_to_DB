5ae52f6f567bbdc6dc352ac13f107c427b8dd0f6
tints-ndlovu <tintswalotndlovu@gmail.com>
2025-08-17 20:11:02 +0200

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Switch to the database
+USE alx_book_store;
+
+-- Insert single row into Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
